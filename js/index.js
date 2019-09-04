3c78d133c3ce81d58cb536209a6894cb2118b893
kiyani <bamba.kiyani@gmail.com>
2019-09-04 08:07:16 -0400

    Create 2 unique event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+// Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
+// 	* * [ ] `mouseover`
+// 	* [ ] `keydown`
+// 	* [ ] `wheel`
+// 	* [ ] `drag / drop`
+// 	* [ ] `load`
+// 	* [ ] `focus`
+// 	* [ ] `resize`
+// 	* [ ] `scroll`
+// 	* [ ] `select`
+//  * [ ] `dblclick`
+    
+// nav.onclick ('mouseover');
+
+document.addEventListener('click', () => { 
+    console.log("it worked")   
+  });
+  
+  
